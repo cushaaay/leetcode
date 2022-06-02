5db732a93d7424c3a2689f04944d6998f8ad89f5
cushaaay <97027161+cushaaay@users.noreply.github.com>
2022-06-02 16:42:03 +0800

    init

---
+++ 0002.js
@@ -0,0 +1,9 @@
+var isPalindrome = function(x) {
+  if (x < 0 || (x !== 0 && x % 10 === 0)) return false;
+  let rev = 0;
+  while (x > rev) {
+    rev = rev * 10 + x % 10;
+    x = parseInt(x / 10);
+  }
+  return (x === rev || x === parseInt(rev / 10));
+};
