2aef16e22d9fe3f0d44a833b0de72d88c573ffae
cushaaay <97027161+cushaaay@users.noreply.github.com>
2022-06-28 08:40:34 +0800

    init

---
+++ 0118.js
@@ -0,0 +1,14 @@
+var generate = function(numRows) {
+  let res = [[1]];
+  for (let i = 0; i < numRows - 1; i++) {
+    let last = res.length - 1;
+    let temp = [0, ...res[last], 0];
+    let row = [];
+    for (let j = 0; j < res[last].length + 1; j++) {
+      row.push(temp[j] + temp[j + 1]);
+    }
+    res.push(row);
+  } 
+  
+  return res;
+};
