52932be0c6efbce531857817f4d0000b7e10d64a
cushaaay <cushaaay@gmail.com>
2022-08-16 19:01:03 +0800

    init 0392

---
+++ solutions/0301-0400/0392/0392.js
@@ -0,0 +1,11 @@
+var isSubsequence = function(s, t) {
+  let index = 0;
+  
+  for (let i = 0; i < t.length; i++) {
+    if (s[index] === t[i]) {
+      index++
+    }
+  }
+  
+  return index === s.length;
+};
